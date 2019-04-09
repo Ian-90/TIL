2e77362201128d26831d0be2e53d0fa6cf331bb5
LeeYoHan <iyohan@LeeYoHanui-MacBookPro.local>
2019-04-09 15:59:33 +0900

    complete lecture 2-2

--- React/movie_app/src/App.js
+++ React/movie_app/src/App.js
@@ -2,14 +2,28 @@ import React, { Component } from 'react';
 import './App.css';
 import Movie from './components/Movie';
 
+const movieTitles = [
+  'Matrix',
+  'Full Metal Jacket',
+  'Oldboy',
+  'Star Wars'
+]
+
+const movieImages = [
+  'http://ojsfile.ohmynews.com/down/images/1/ctzxp_249945_1[363282].jpg',
+  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoF48z3ClBQhzf4wr301zI8b_prryzc2xHatueKEC5ZDXxTFsJ',
+  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNJCbvPYORxcd8jgGWMsosNeXIRVmI7ptMrsj3Dv81Rea1D2JU',
+  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDhhUig0ltFxr99G4LY90vweM34VJOoNoyvBaP-0Nm6MZSdhoabA'  
+]
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Movie />
-        <Movie />
-        <Movie />
-        <Movie />
+        <Movie title={movieTitles[0]} poster={movieImages[0]} />
+        <Movie title={movieTitles[1]} poster={movieImages[1]} />
+        <Movie title={movieTitles[2]} poster={movieImages[2]} />
+        <Movie title={movieTitles[3]} poster={movieImages[3]} />
       </div>
     );
   }
--- React/movie_app/src/components/Movie.js
+++ React/movie_app/src/components/Movie.js
@@ -5,10 +5,11 @@ import './Movie.css'
 
 class Movie extends Component {
   render() {
+    console.log(this.props)
     return (
       <div>
-        <h1>Hello This is a Movie</h1>  
-        <MoviePoster />
+        <h1>{this.props.title}</h1>  
+        <MoviePoster poster={this.props.poster} />
       </div>
     );
   }
@@ -16,9 +17,10 @@ class Movie extends Component {
 
 class MoviePoster extends Component {
   render() {
+    console.log(this.props)
     return (
       <div>
-        <img src="https://post-phinf.pstatic.net/MjAxODEyMThfMTM2/MDAxNTQ1MTE1Mzg5NTk3.uM82sQH5oOsdbPElrNIR3R8yPyPKWUOANMC2pEkBZSgg.rHuoP7uYPD0YX23AlqVzA-rJ7dw6cSCciUa2fMhwW0og.PNG/image_2004770701545021747352.png?type=w1200" />
+        <img src={this.props.poster} />
       </div>
     );
   }
