ec9cecbe5049dd3eb222e75d8ae401a654ad3954
LeeYoHan <iyohan@LeeYoHanui-MacBookPro.local>
2019-04-14 00:53:40 +0900

    complete lecture 4-2

--- React/movie_app/src/App.js
+++ React/movie_app/src/App.js
@@ -2,43 +2,48 @@ import React, { Component } from 'react';
 import './App.css';
 import Movie from './components/Movie';
 
-const movies = [
-  {
-    title: 'Matrix',
-    poster: 'http://ojsfile.ohmynews.com/down/images/1/ctzxp_249945_1[363282].jpg',
-  },
-  {
-    title: 'Full Metal Jacket',
-    poster: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoF48z3ClBQhzf4wr301zI8b_prryzc2xHatueKEC5ZDXxTFsJ'
-  },
-  {
-    title: 'Oldboy',
-    poster: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNJCbvPYORxcd8jgGWMsosNeXIRVmI7ptMrsj3Dv81Rea1D2JU'
-  },
-  {
-    title: 'Star Wars',
-    poster: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDhhUig0ltFxr99G4LY90vweM34VJOoNoyvBaP-0Nm6MZSdhoabA'
-  }
-]
-
 class App extends Component {
   state = {
-    greeting: 'Hellog'
+    greeting: 'Hellog',
+    movies: [
+      {
+        title: 'Matrix',
+        poster: 'http://ojsfile.ohmynews.com/down/images/1/ctzxp_249945_1[363282].jpg',
+      },
+      {
+        title: 'Full Metal Jacket',
+        poster: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoF48z3ClBQhzf4wr301zI8b_prryzc2xHatueKEC5ZDXxTFsJ'
+      },
+      {
+        title: 'Oldboy',
+        poster: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNJCbvPYORxcd8jgGWMsosNeXIRVmI7ptMrsj3Dv81Rea1D2JU'
+      },
+      {
+        title: 'Star Wars',
+        poster: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDhhUig0ltFxr99G4LY90vweM34VJOoNoyvBaP-0Nm6MZSdhoabA'
+      }
+    ]
   }
 
   componentDidMount = () => {
     setTimeout(() => {
       this.setState({
-        greeting: 'Hello again!'
+        movies: [
+          ...this.state.movies,
+          {
+            title: 'Transptting',
+            poster: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVlyMKmtyf03kKhrRDYmFj-qQTJfiRbEoPgsxwi1T9n2fKGDHT'
+          }
+        ]
       })
     }, 5000)
   }
-  
+
   render() {
     return (
       <div className="App">
         {this.state.greeting}
-        {movies.map((movie, idx) => {
+        {this.state.movies.map((movie, idx) => {
           return <Movie title={movie.title} poster={movie.poster} key={idx} />
         })}
       </div>
