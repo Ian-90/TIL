caaae354194ffd91a96d8db24af4dff7df32a177
LeeYoHan <iyohan@LeeYoHanui-MacBookPro.local>
2019-04-14 12:28:38 +0900

    complete lecture 6-1

--- React/movie_app/src/App.js
+++ React/movie_app/src/App.js
@@ -8,32 +8,7 @@ class App extends Component {
   }
 
   componentDidMount = () => {
-    setTimeout(() => {
-      this.setState({
-        movies: [
-          {
-            title: 'Matrix',
-            poster: 'http://ojsfile.ohmynews.com/down/images/1/ctzxp_249945_1[363282].jpg',
-          },
-          {
-            title: 'Full Metal Jacket',
-            poster: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoF48z3ClBQhzf4wr301zI8b_prryzc2xHatueKEC5ZDXxTFsJ'
-          },
-          {
-            title: 'Oldboy',
-            poster: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNJCbvPYORxcd8jgGWMsosNeXIRVmI7ptMrsj3Dv81Rea1D2JU'
-          },
-          {
-            title: 'Star Wars',
-            poster: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDhhUig0ltFxr99G4LY90vweM34VJOoNoyvBaP-0Nm6MZSdhoabA'
-          },
-          {
-            title: 'Transptting',
-            poster: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVlyMKmtyf03kKhrRDYmFj-qQTJfiRbEoPgsxwi1T9n2fKGDHT'
-          }
-        ]
-      })
-    }, 5000)
+    fetch('https://yts.am/api/v2/list_movies.json?sort_by=download_count')
   }
 
   renderMovies = () => {
