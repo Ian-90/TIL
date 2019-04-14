04eb2a8717f56301cd302c8d73d2c3f1ba3b5de5
LeeYoHan <iyohan@LeeYoHanui-MacBookPro.local>
2019-04-14 23:31:39 +0900

    complete lecture 7-1

--- React/movie_app/src/App.js
+++ React/movie_app/src/App.js
@@ -13,7 +13,13 @@ class App extends Component {
 
   renderMovies = () => {
     const movies = this.state.movies.map(movie => {
-      return <Movie title={movie.title} poster={movie.large_cover_image} key={movie.id} />
+      return <Movie 
+                title={movie.title_english} 
+                poster={movie.medium_cover_image} 
+                key={movie.id} 
+                genres={movie.genres}
+                synopsis={movie.synopsis} 
+              />
     })
 
     return movies
--- React/movie_app/src/components/Movie.js
+++ React/movie_app/src/components/Movie.js
@@ -2,28 +2,51 @@ import React from 'react';
 import './Movie.css'
 import PropTypes from 'prop-types';
 
-const Movie = ({title, poster}) => {
+const Movie = ({title, poster, genres, synopsis}) => {
   return (
-    <div>
-      <h1>{title}</h1>  
-      <MoviePoster poster={poster} />
+    <div className='Movie'>
+      <div className='Movie_Columns'>
+        <MoviePoster poster={poster} alt={title} />
+      </div>
+      <div className='Movie_Columns'>
+        <h1>{title}</h1>
+        <div className='Movie_Genres'>
+          {genres.map((genre, idx) => <MovieGenre genre={genre} key={idx} />)}
+        </div>
+        <p className='Movie_Synopsis'>
+          {synopsis}
+        </p>
+      </div>
     </div>
   )
 }
 
-const MoviePoster = ({poster}) => {
+const MoviePoster = ({poster, alt}) => {
   return (
-    <img src={poster} alt='Movie Poster' />
+    <img src={poster} alt={alt} title={alt} className='Movie_Poster' />
+  )
+}
+
+const MovieGenre = ({genre}) => {
+  return (
+    <span className='Movie_Genre'>{genre}</span>
   )
 }
 
 Movie.propTypes = {
   title: PropTypes.string.isRequired,
-  poster: PropTypes.string.isRequired
+  poster: PropTypes.string.isRequired,
+  genres: PropTypes.array.isRequired,
+  synopsis: PropTypes.string.isRequired
 }
 
 MoviePoster.propTypes = {
-  poster: PropTypes.string.isRequired
+  poster: PropTypes.string.isRequired,
+  alt: PropTypes.string.isRequired
+}
+
+MovieGenre.propTypes = {
+  genre: PropTypes.string.isRequired
 }
 
 export default Movie;
