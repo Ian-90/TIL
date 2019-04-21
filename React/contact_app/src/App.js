96a7aabf21e7473c26e4c6b37ea2100bc9d4a2ba
LeeYoHan <iyohan@LeeYoHanui-MacBookPro.local>
2019-04-22 00:38:22 +0900

    complete lecture 9-1

--- React/contact_app/src/App.js
+++ React/contact_app/src/App.js
@@ -5,18 +5,35 @@ import PhoneInfoList from './components/PhoneInfoList';
 
 class App extends Component {
 
-  id = 0;
+  id = 3;
 
   state = {
-    infomation: []
+    infomation: [
+      {
+        id: 0,
+        name: '홍길동',
+        phone: '010-0000-0001'
+      },
+      {
+        id: 1,
+        name: '이안',
+        phone: '010-0000-0002'
+      },
+      {
+        id: 2,
+        name: '이어안',
+        phone: '010-0000-0003'
+      }
+    ]
   }
 
   handleCreate = (data) => {
     const { infomation } = this.state
     this.setState({
-      infomation: infomation.concat(Object.assign({}, data, {
+      infomation: infomation.concat({
+        ...data,
         id: this.id++
-      }))
+      })
     })
   }
 
