621d80139586a746ec206cab14cade0b062ef872
LeeYoHan <iyohan@LeeYoHanui-MacBookPro.local>
2019-04-22 00:42:14 +0900

    complete lecture 9-2

--- React/contact_app/src/App.js
+++ React/contact_app/src/App.js
@@ -24,7 +24,14 @@ class App extends Component {
         name: '이어안',
         phone: '010-0000-0003'
       }
-    ]
+    ],
+    keyword: ''
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      keyword: e.target.value
+    })
   }
 
   handleCreate = (data) => {
@@ -65,8 +72,15 @@ class App extends Component {
     return (
       <div>
         <PhoneForm onCreate={this.handleCreate} />
+        <input 
+          value={this.state.keyword}
+          onChange={this.handleChange}
+          placeholder='검색...'
+        />
         <PhoneInfoList 
-          data={this.state.infomation} 
+          data={this.state.infomation.filter(
+            info => info.name.indexOf(this.state.keyword) > -1
+          )} 
           onRemove={this.handleRemove}
           onUpdate={this.handleUpdate}
         />
