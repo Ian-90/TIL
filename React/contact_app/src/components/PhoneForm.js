bfc6acb5bb76a9d7144c7fd421d0d23655ff2649
LeeYoHan <iyohan@LeeYoHanui-MacBookPro.local>
2019-04-22 00:46:26 +0900

    complete lecture 9-3

--- React/contact_app/src/components/PhoneForm.js
+++ React/contact_app/src/components/PhoneForm.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 
 class PhoneForm extends Component {
+  input = React.createRef();
+
   state = {
     name: '',
     phone: ''
@@ -22,6 +24,7 @@ class PhoneForm extends Component {
       name: '',
       phone: ''
     })
+    this.input.current.focus();
   }
 
   render() {
@@ -32,6 +35,7 @@ class PhoneForm extends Component {
           placeholder='이름' 
           onChange={this.handleChange} 
           value={this.state.name} 
+          ref={this.input}
         />
         <input 
           name='phone'
