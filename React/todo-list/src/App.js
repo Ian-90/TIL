d3768377c73de2df2c1dc1d343988efe81f614a5
LeeYoHan <yohan4551@gmail.com>
2019-04-25 22:39:10 +0900

    add TodoListTemplate Component

--- React/todo-list/src/App.js
+++ React/todo-list/src/App.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
+import TodoListTemplate from './components/TodoListTemplate';
 
-
-function App() {
-  return (
-    <div className="App">
-      App
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <TodoListTemplate>
+        템플릿 완성
+      </TodoListTemplate>
+    );
+  }
 }
 
 export default App;
