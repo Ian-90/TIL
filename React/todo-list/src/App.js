eb835c916a4a57465a25a9d90bd4e0524d387aa8
LeeYoHan <yohan4551@gmail.com>
2019-04-25 23:15:44 +0900

    add TodoItem Component

--- React/todo-list/src/App.js
+++ React/todo-list/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import TodoListTemplate from './components/TodoListTemplate';
 import Form from './components/Form';
+import TodoList from './components/TodoItemList';
 
 class App extends Component {
   render() {
     return (
       <TodoListTemplate form={<Form />}>
-        템플릿 완성
+        <TodoList />
       </TodoListTemplate>
     );
   }
--- React/todo-list/src/components/TodoItemList.js
+++ React/todo-list/src/components/TodoItemList.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import TodoItem from './TodoItem';
 
 class TodoItemList extends Component {
   render() {
     const { todos, onToggle, onRemove } = this.props;
     return (
       <div>
-        
+        <TodoItem text='hello' />
+        <TodoItem text='react' />
+        <TodoItem text='nice to meet you' />
       </div>
     );
   }
