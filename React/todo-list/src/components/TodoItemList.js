508f4ebab5222f578ad50dea26096d43464099a9
LeeYoHan <yohan4551@gmail.com>
2019-04-25 22:54:14 +0900

    add TodoItemList Component

---
+++ React/todo-list/src/components/TodoItemList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class TodoItemList extends Component {
+  render() {
+    const { todos, onToggle, onRemove } = this.props;
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default TodoItemList;
