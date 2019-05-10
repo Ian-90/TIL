d915ee594480eba998a112997a7ecab9492c5e91
LeeYoHan <>
2019-05-10 20:36:57 +0900

    complete lecture 2-3 browser router and exact and Redirect and Switch

--- React/nomflix/src/components/App.js
+++ React/nomflix/src/components/App.js
@@ -1,12 +1,14 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import Router from 'components/Router';
+import Header from 'components/Header';
 
 class App extends Component {
   render() {
     return (
-      <Fragment>
+      <>
+        <Header />
         <Router />
-      </Fragment>
+      </>
     );
   }
 }
--- React/nomflix/src/components/Router.js
+++ React/nomflix/src/components/Router.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import { HashRouter as Router, Route} from "react-router-dom";
+import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
 import Home from "Routes/Home";
 import TV from "Routes/TV";
 import Search from "Routes/Search";
 
 export default () => (
   <Router>
-    <Route path="/" exact component={Home} />
-    <Route path="/tv" exact component={TV} />
-    <Route path="/search" exact component={Search} />
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route path="/tv" exact component={TV} />
+      <Route path="/search" exact component={Search} />
+      <Redirect from="*" to="/" />
+    </Switch> 
   </Router>
 )
