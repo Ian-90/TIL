a6a873164be7e8b4180c39833f2bd2333b91f428
LeeYoHan <>
2019-08-24 00:03:50 +0900

    complete lecture 5-7 Detail Container part Two

--- React/nomflix/src/Routes/Detail/DetailContainer.js
+++ React/nomflix/src/Routes/Detail/DetailContainer.js
@@ -1,11 +1,19 @@
 import React from "react";
 import DetailPresenter from './DetailPresenter';
+import { moviesApi, tvApi } from "../../api";
 
 export default class extends React.Component {
-  state = {
-    result: null,
-    error: null,
-    loading: true
+  constructor(props) {
+    super(props);
+    const { 
+      location: { pathname } 
+    } = props;
+    this.state = {
+      result: null,
+      error: null,
+      loading: true,
+      isMovie: pathname.includes("/movie/")      
+    }
   }
 
   async componentDidMount() {
@@ -15,9 +23,29 @@ export default class extends React.Component {
       },
       history: { push }
     } = this.props;
+    const { isMovie } = this.state;
     const parseId = parseInt(id);
     if (isNaN(parseId)) {
       return push("/")
+    }
+    let result = null;
+    try {
+      if (isMovie) {
+        const request = await moviesApi.movieDetail(parseId);
+        result = request.data;
+      } else {
+        const request = await tvApi.showDetail(parseId); 
+        result = request.data;
+      }
+    } catch {
+      this.setState({
+        error: "Can't find anything."
+      })
+    } finally {
+      this.setState({
+        loading: false,
+        result
+      })
     }
   }
 
