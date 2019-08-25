0b656ab874e96b6427c85b374b9ec6e691350c91
LeeYoHan <>
2019-08-25 23:51:58 +0900

    complete lecture 6-1 Presenter Structure

--- React/nomflix/src/Routes/Home/HomePresenter.js
+++ React/nomflix/src/Routes/Home/HomePresenter.js
@@ -1,1 +1,15 @@
-export default () => "Home";
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const HomePresenter = ({ nowPlaying, upcoming, popular, error, loading}) => null ;
+
+HomePresenter.propTypes = {
+  nowPlaying: PropTypes.array,
+  upcoming: PropTypes.array,
+  popular: PropTypes.array,
+  loading: PropTypes.bool.isRequired,
+  error: PropTypes.string
+}
+
+export default HomePresenter;
--- React/nomflix/src/Routes/Search/SearchPresenter.js
+++ React/nomflix/src/Routes/Search/SearchPresenter.js
@@ -1,1 +1,16 @@
-export default () => "Search";
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const SearchPresenter = ({ movieResults, tvResults, searchTerm, handleSubmit, error, loading}) => null ;
+
+SearchPresenter.propTypes = {
+  movieResults: PropTypes.array,
+  tvResults: PropTypes.array,
+  searchTerm: PropTypes.string,
+  handleSubmit: PropTypes.func.isRequired,
+  loading: PropTypes.bool.isRequired,
+  error: PropTypes.string,
+}
+
+export default SearchPresenter;
