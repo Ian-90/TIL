5f9ee9a180e246a0ae4986b4f3e7d4499279f31c
LeeYoHan <>
2019-08-26 00:15:38 +0900

    complete lecture 6-2 HomePresenter and Section Components

--- React/nomflix/src/Routes/Home/HomePresenter.js
+++ React/nomflix/src/Routes/Home/HomePresenter.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import Section from 'Components/Section';
 
-const HomePresenter = ({ nowPlaying, upcoming, popular, error, loading}) => null ;
+const Container = styled.div`
+padding: 0px 10px;
+`;
+
+const HomePresenter = ({ nowPlaying, upcoming, popular, error, loading}) => (
+  loading ? null :
+  <Container>
+    {nowPlaying && nowPlaying.length > 0 && <Section title="Now Playing">
+      {nowPlaying.map(movie => movie.title)}
+    </Section>}
+    {upcoming && upcoming.length > 0 && <Section title="Upcoming Playing">
+      {upcoming.map(movie => movie.title)}
+    </Section>}
+    {popular && popular.length > 0 && <Section title="Popular Movies">
+      {popular.map(movie => movie.title)}
+    </Section>}
+  </Container>
+)
 
 HomePresenter.propTypes = {
   nowPlaying: PropTypes.array,
