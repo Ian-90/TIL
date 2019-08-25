36e7086bc801db6a9604dd74993687f804a6d32d
LeeYoHan <>
2019-08-26 00:41:19 +0900

    complete lecture 6-3 TVPresenter and Loader Components

--- React/nomflix/src/Routes/TV/TVPresenter.js
+++ React/nomflix/src/Routes/TV/TVPresenter.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import Section from 'Components/Section';
+import Loader from 'Components/Loader';
 
-const TVPresenter = ({ topRated, popular, airingToday, error, loading}) => null ;
+const Container = styled.div`
+  padding: 0px 10px;
+`;
+
+const TVPresenter = ({ topRated, popular, airingToday, error, loading}) => (
+  loading ? 
+    <Loader /> :
+    <Container>
+      {topRated && topRated.length > 0 && <Section title="Top Rated Shows">{topRated.map(show => show.name)}</Section>}
+      {popular && popular.length > 0 && <Section title="Popular Shows">{popular.map(show => show.name)}</Section>}
+      {airingToday && airingToday.length > 0 && <Section title="Airing Today">{airingToday.map(show => show.name)}</Section>}    
+    </Container>
+) ;
 
 TVPresenter.propTypes = {
   topRated: PropTypes.array,
