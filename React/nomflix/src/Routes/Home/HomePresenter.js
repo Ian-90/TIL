62c5cfde0f4fab219f015c2021f1c8355796f6ef
LeeYoHan <yohan4551@gmail.com>
2019-09-02 00:10:03 +0900

    complete lecture 6-7 Rendering Poster Component

--- React/nomflix/src/Routes/Home/HomePresenter.js
+++ React/nomflix/src/Routes/Home/HomePresenter.js
@@ -14,13 +14,43 @@ const HomePresenter = ({ nowPlaying, upcoming, popular, error, loading}) => (
   loading ? <Loader /> :
   <Container>
     {nowPlaying && nowPlaying.length > 0 && <Section title="Now Playing">
-      {nowPlaying.map(movie => <Poster />)}
+      {nowPlaying.map(movie => (
+        <Poster 
+          key={movie.id}
+          id={movie.id} 
+          title={movie.original_title} 
+          imageUrl={movie.poster_path}
+          rating={movie.vote_average} 
+          isMovie={true}
+          year={movie.release_date && movie.release_date.substring(0, 4)}
+        />
+      ))}
     </Section>}
     {upcoming && upcoming.length > 0 && <Section title="Upcoming Playing">
-      {upcoming.map(movie => <Poster />)}
+      {upcoming.map(movie => (
+        <Poster 
+          key={movie.id} 
+          id={movie.id}
+          title={movie.original_title} 
+          imageUrl={movie.poster_path}
+          rating={movie.vote_average} 
+          isMovie={true}
+          year={movie.release_date && movie.release_date.substring(0, 4)}
+        />
+      ))}
     </Section>}
     {popular && popular.length > 0 && <Section title="Popular Movies">
-      {popular.map(movie => <Poster />)}
+      {popular.map(movie => (
+        <Poster 
+          key={movie.id} 
+          id={movie.id}
+          title={movie.original_title} 
+          imageUrl={movie.poster_path}
+          rating={movie.vote_average} 
+          isMovie={true}
+          year={movie.release_date && movie.release_date.substring(0, 4)}
+        />
+      ))}
     </Section>}
     {error && <Message color='#e74c3c' text={error} />}
   </Container>
--- React/nomflix/src/Routes/TV/TVPresenter.js
+++ React/nomflix/src/Routes/TV/TVPresenter.js
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 import Section from 'Components/Section';
 import Loader from 'Components/Loader';
 import Message from 'Components/Message';
+import Poster from 'Components/Poster';
 
 const Container = styled.div`
   padding: 0px 20px;
@@ -13,9 +14,48 @@ const TVPresenter = ({ topRated, popular, airingToday, error, loading}) => (
   loading ? 
     <Loader /> :
     <Container>
-      {topRated && topRated.length > 0 && <Section title="Top Rated Shows">{topRated.map(show => show.name)}</Section>}
-      {popular && popular.length > 0 && <Section title="Popular Shows">{popular.map(show => show.name)}</Section>}
-      {airingToday && airingToday.length > 0 && <Section title="Airing Today">{airingToday.map(show => show.name)}</Section>}   
+      {topRated && topRated.length > 0 && (
+        <Section title="Top Rated Shows">
+          {topRated.map(show => (
+            <Poster 
+              key={show.id}
+              id={show.id} 
+              title={show.original_name} 
+              imageUrl={show.poster_path}
+              rating={show.vote_average} 
+              year={show.first_air_date && show.first_air_date.substring(0, 4)}
+            />
+          ))}
+        </Section>
+      )}
+      {popular && popular.length > 0 && (
+        <Section title="Popular Shows">
+          {popular.map(show => (
+            <Poster 
+              key={show.id}
+              id={show.id} 
+              title={show.original_name} 
+              imageUrl={show.poster_path}
+              rating={show.vote_average} 
+              year={show.first_air_date && show.first_air_date.substring(0, 4)}
+            />
+          ))}
+        </Section>
+      )}
+      {airingToday && airingToday.length > 0 && (
+        <Section title="Airing Today">
+          {airingToday.map(show => (
+            <Poster 
+              key={show.id}
+              id={show.id} 
+              title={show.original_name} 
+              imageUrl={show.poster_path}
+              rating={show.vote_average} 
+              year={show.first_air_date && show.first_air_date.substring(0, 4)}
+            />
+          ))}
+        </Section>
+      )}   
       {error && <Message color='#e74c3c' text={error} />}
     </Container>
 ) ;
