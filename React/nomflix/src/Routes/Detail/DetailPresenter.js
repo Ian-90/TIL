51cb8c2b83b3062052425c5ea9bd5be4555deef7
LeeYoHan <yohan4551@gmail.com>
2019-09-08 01:43:50 +0900

    complete lecture 6-9 Detail Container part One

--- React/nomflix/src/Routes/Detail/DetailPresenter.js
+++ React/nomflix/src/Routes/Detail/DetailPresenter.js
@@ -1,8 +1,61 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import Loader from 'Components/Loader';
 
-const DetailPresenter = ({ result, error, loading}) => null ;
+const Container = styled.div`
+  height: calc(100vh - 50px);
+  width: 100%;
+  position: relative;
+  padding: 50px;
+`
+
+const Backdrop = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-image: url(${props => props.bgImage});
+  background-position: center center;
+  background-size: cover;
+  filter: blur(3px);
+  opacity: 0.5;
+  z-index: 0;
+`
+const Content = styled.div`
+  display: flex;
+  width: 100%;
+  position: relative;
+  z-index: 1;
+  height: 100%;
+`;
+
+const Cover = styled.div`
+  width: 30%;
+  background-image: url(${props => props.bgImage});
+  background-position: center center;
+  background-size: cover;
+  height: 100%;
+  border-radius: 5px;
+`;
+
+const DetailPresenter = ({ result, error, loading}) => (
+  loading ? (
+    <Loader />
+  ) : (
+  <Container>
+    {console.log('@',result.backdrop_path)}
+    <Backdrop bgImage={`https://image.tmdb.org/t/p/original${result.backdrop_path}`} />
+    <Content>
+      <Cover 
+        bgImage={
+          result.poster_path 
+          ? `https://image.tmdb.org/t/p/original${result.poster_path}` 
+          : require('../../assets/noPosterSmall.png')} />
+    </Content>
+  </Container>)
+) ;
 
 DetailPresenter.propTypes = {
   result: PropTypes.object,
