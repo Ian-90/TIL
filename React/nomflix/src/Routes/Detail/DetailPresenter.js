4d93dad532563d3891d5b1d802bf2fcbb95feeb8
LeeYoHan <yohan4551@gmail.com>
2019-09-09 00:14:14 +0900

    complete lecture 6-10 Detail Container part Two

--- React/nomflix/src/Routes/Detail/DetailPresenter.js
+++ React/nomflix/src/Routes/Detail/DetailPresenter.js
@@ -40,19 +40,71 @@ const Cover = styled.div`
   border-radius: 5px;
 `;
 
+const Data = styled.div`
+  width: 70%;
+  margin-left: 10px;
+`;
+
+const Title = styled.h3`
+  font-size: 32px;
+`;
+
+const ItemContainer = styled.div`
+  margin: 20px 0;
+`;
+
+const Item = styled.span``;
+
+const Divider = styled.span`
+  margin: 0 10px;
+`;
+
+const Overview = styled.p`
+  font-size: 12px;
+  opacity: 0.7;
+  line-height: 1.5;
+  width: 50%;
+`;
+
 const DetailPresenter = ({ result, error, loading}) => (
   loading ? (
     <Loader />
   ) : (
   <Container>
-    {console.log('@',result.backdrop_path)}
     <Backdrop bgImage={`https://image.tmdb.org/t/p/original${result.backdrop_path}`} />
     <Content>
       <Cover 
         bgImage={
           result.poster_path 
           ? `https://image.tmdb.org/t/p/original${result.poster_path}` 
-          : require('../../assets/noPosterSmall.png')} />
+          : require('../../assets/noPosterSmall.png')} 
+      />
+      <Data>
+        <Title>{result.original_title ? result.original_title: result.original_name}</Title>
+        <ItemContainer>
+          <Item>
+            {result.release_date 
+              ? result.release_date.substring(0, 4) 
+              : result.first_air_date.substring(0, 4)}
+          </Item>
+          <Divider>∙</Divider>
+          <Item>
+            {result.runtime 
+              ? result.runtime 
+              : result.episode_run_time[0]} min
+          </Item>
+          <Divider>∙</Divider>
+          <Item>
+            {result.genres && 
+              result.genres.map((genre, idx) => 
+                idx === result.genres.length - 1 
+                  ? genre.name 
+                  : `${genre.name} /`
+            )}
+          </Item>
+        </ItemContainer>
+        <Overview>{result.overview}</Overview>
+      </Data>
     </Content>
   </Container>)
 ) ;
