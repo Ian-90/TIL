d83fc541ed5215daa9e560a1432bc8814326f955
LeeYoHan <yohan4551@gmail.com>
2019-09-09 23:56:40 +0900

    Updates

---
+++ precache-manifest.daa6cb6a510b78b5006a58815136c254.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c1c1252b87498fd542a87add965bf3d",
+    "url": "%20Ian-90.github.io/nomflix/index.html"
+  },
+  {
+    "revision": "202afbc7ce480c2abf0d",
+    "url": "%20Ian-90.github.io/nomflix/static/js/2.ab785bc3.chunk.js"
+  },
+  {
+    "revision": "79452404368c8b18e233",
+    "url": "%20Ian-90.github.io/nomflix/static/js/main.379573c0.chunk.js"
+  },
+  {
+    "revision": "e0bee62833cea133086f",
+    "url": "%20Ian-90.github.io/nomflix/static/js/runtime~main.f42287c4.js"
+  },
+  {
+    "revision": "6df993a6f44ca8a00a7fca436ac06235",
+    "url": "%20Ian-90.github.io/nomflix/static/media/noPosterSmall.6df993a6.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/movie_app/precache-manifest.28864f36211b288e02f1d0852e48b62a.js"
+  "%20Ian-90.github.io/nomflix/precache-manifest.daa6cb6a510b78b5006a58815136c254.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/movie_app/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("%20Ian-90.github.io/nomflix/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
