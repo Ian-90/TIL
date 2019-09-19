0cac1743de406cca55f0b6f52879400896e976aa
LeeYoHan <yohan4551@gmail.com>
2019-09-20 00:28:44 +0900

    lecture summary 15-2

---
+++ JavaScript/ES6/15.미니프로젝트/webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.js', // 빌드할때 바라볼곳.
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [{
+
+    }]
+  }
+}
