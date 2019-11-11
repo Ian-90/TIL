549d4e829963f8373e19b786a1719a634ba8300c
LeeYoHan <yohan4551@gmail.com>
2019-11-12 00:30:24 +0900

    lecture summary 15-4

--- JavaScript/ES6/15.미니프로젝트/webpack.config.js
+++ JavaScript/ES6/15.미니프로젝트/webpack.config.js
@@ -4,13 +4,14 @@ module.exports = {
   entry: './src/index.js', // 빌드할때 바라볼곳.
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/dist'
   },
   module: {
     rules: [
       {
         test: /\.js$/,
-        exclude: "/node_modules",
+        exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
         }
