688ad5179a69a1fbc9b8e77da3334da484cc234e
LeeYoHan <yohan4551@gmail.com>
2020-02-16 17:32:23 +0900

    lecture summary 2-3 ~ 2-4

---
+++ JavaScript/inflearn_tdd/tdd_project/spec/ClickCountView.spec.js
@@ -0,0 +1,34 @@
+const App = require("../src/components/ClickCounter");
+const ViewApp = require("../src/components/ClickCountView");
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+const { document } = new JSDOM(`<!DOCTYPE html><span></span>)`).window;
+
+describe("App.ClickCountView 모듈", () => {
+  let clickCounter, updateEl, view;
+  beforeEach(() => {
+    clickCounter = App.ClickCounter();
+    updateEl = document.createElement("span");
+    view = ViewApp.ClickCountView(clickCounter, updateEl);
+  });
+
+  it("clickCounter를 주입하지 않으면 에러를 던진다.", () => {
+    clickCounter = null;
+    const actual = () => ViewApp.ClickCountView(clickCounter, updateEl);
+    expect(actual).toThrowError("clickCounter");
+  });
+
+  it("updateEl을 주입하지 않으면 에러를 던진다.", () => {
+    updateEl = null;
+    const actual = () => ViewApp.ClickCountView(clickCounter, updateEl);
+    expect(actual).toThrowError("updateEl");
+  });
+
+  describe("updateView()", () => {
+    it("ClickCounter의 getValue() 값을 출력한다.", () => {
+      const counterValue = clickCounter.getValue();
+      view.updateView();
+      expect(updateEl.innerHTML).toBe(counterValue.toString());
+    });
+  });
+});
