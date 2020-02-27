1e74dcb700dd514f4f2abb149832ae5b10c7100f
LeeYoHan <yohan4551@gmail.com>
2020-02-28 00:56:03 +0900

    udemy react section3 ~ section4 complete

---
+++ React/udemy/ReduxSimpleStarter/src/reducers/reducer_book.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: "Javascript", pages: 101 },
+    { title: "Harry Poerter", pages: 39 },
+    { title: "The Dark Tower", pages: 85 },
+    { title: "Eloquent Ruby", pages: 1 }
+  ];
+}
