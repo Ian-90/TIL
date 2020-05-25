14a5d9a669148e1e7074ddbffedefe05709d9399
LeeYoHan <yohan4551@gmail.com>
2020-05-26 00:19:24 +0900

    complete lecture 4

--- JavaScript/nomad_typescript/README.md
+++ JavaScript/nomad_typescript/README.md
@@ -20,3 +20,22 @@ yarn global add typescript
   ```
   npx tsc [파일명]
   ```
+
+* 계속 컴파일 하려면 ? 컴파일 명령어에 -w 또는 --watch 옵션을 붙인다.
+```
+tsc [파일명] --watch
+```
+
+## Types
+1. function
+```
+const func(arguments: type, ...): return type => {}
+```
+
+* example
+```javascript
+const sayHi = (name: string, age: number, gender: string): string => {
+  return `Hello ${name}, you are ${age}, you are a ${gender}`
+}
+
+```
--- JavaScript/nomad_typescript/dist/index.js
+++ JavaScript/nomad_typescript/dist/index.js
@@ -1,8 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const name = "Ian", age = 24, gender = "male";
 const sayHi = (name, age, gender) => {
-    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+    return `Hello ${name}, you are ${age}, you are a ${gender}!!`;
 };
-sayHi(name, age);
+console.log(sayHi("Ian", 24, "male"));
 //# sourceMappingURL=index.js.map
