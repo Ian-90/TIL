062a579d0b369762eb5fb737c8d752472a903140
LeeYoHan <yohan4551@gmail.com>
2020-05-26 00:56:26 +0900

    complete lecture 7

--- JavaScript/nomad_typescript/src/index.ts
+++ JavaScript/nomad_typescript/src/index.ts
@@ -1,20 +1,28 @@
-class Human {
-  public name: string;
-  public age: number;
-  public gender: string;
-  constructor(name: string, age: number, gender: string) {
-    this.name = name;
-    this.age = age;
-    this.gender = gender;
+class Block {
+  public index: number;
+  public hash: string;
+  public previousHash: string;
+  public data: string;
+  public timestamp: number;
+  constructor(
+    index: number,
+    hash: string,
+    previousHash: string,
+    data: string,
+    timestamp: number
+  ) {
+    this.index = index
+    this.hash = hash
+    this.previousHash = previousHash
+    this.data = data
+    this.timestamp = timestamp
   }
 }
 
-const Ian = new Human("Ian", 22, "male")
+const genesisBlock:Block = new Block(0, "2020202020202", "", "Hello", 123456);
 
-const sayHi = (person: Human): string => {
-  return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!!`
-}
+let blockchain: [Block] = [genesisBlock];
 
-console.log(sayHi(Ian))
+console.log(blockchain)
 
 export {};
