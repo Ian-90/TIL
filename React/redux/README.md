2096537f72b031a3cdfea220f46c6c6a83f0c9f5
LeeYoHan <yohan4551@gmail.com>
2020-05-29 01:24:48 +0900

    complete lecture 1-1 ~ 1-4

---
+++ React/redux/README.md
@@ -0,0 +1,17 @@
+# NOMAD CODERS
+
+- [초보자를 위한 리덕스 101](https://academy.nomadcoders.co/p/build-a-timer-app-with-react-native-and-redux)
+
+## 1. Pure Redux: Counter
+
+* install
+```
+yarn add redux
+```
+
+* redux는 데이터를 관리해주기 위해 만들어 졌다.
+
+* store - 데이터를 저장하는 장소
+* reducer - 앱의 데이터를 리턴하는 함수
+* action - reducer와 소통 하는 방법(데이터를 변경하는 함수)
+* dispath - action을 받아서 store에 보내는 함수
---
+++ React/redux/src/index.js
@@ -0,0 +1,33 @@
+import { createStore } from 'redux'
+
+const plus = document.getElementById("add")
+const minus = document.getElementById("minus")
+const number = document.querySelector("span")
+
+const ADD = "ADD"
+const MINUS = "MINUS"
+
+const countModifier = (count = 0, action) => {
+  switch (action.type) {
+    case ADD:
+      return count + 1
+    case MINUS:
+      return count -1
+    default:
+      return count
+  }
+}
+
+const countStore = createStore(countModifier)
+
+const onChange = () => {
+  number.innerText = countStore.getState()
+}
+
+countStore.subscribe(onChange)
+
+const handleAdd = () => countStore.dispatch({ type: ADD })
+const handleMinus = () => countStore.dispatch({ type: MINUS })
+
+plus.addEventListener('click', handleAdd)
+minus.addEventListener('click', handleMinus)
