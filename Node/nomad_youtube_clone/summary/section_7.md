2f179dfa419756dcd45ee56ab24a7eff3ec6daec
LeeYoHan <yohan4551@gmail.com>
2021-06-09 02:07:12 +0900

    feat: section7 - 1 수강완료

---
+++ Node/nomad_youtube_clone/summary/section_7.md
@@ -0,0 +1,4 @@
+## 1. Create Accoun part One
+* User 모델 생성
+* join 컨트롤러 생성
+* join.pug 생성
--- Node/nomad_youtube_clone/wetube/src/controllers/userController.js
+++ Node/nomad_youtube_clone/wetube/src/controllers/userController.js
@@ -1,4 +1,12 @@
-export const join = (req, res) => res.send('Join')
+export const getJoin = (req, res) => {
+  return res.render('join', { pageTitle: 'Join' })
+}
+
+export const postJoin = (req, res) => {
+  console.log('test', req.body)
+  return res.end()
+}
+
 export const edit = (req, res) => res.send('Edit User')
 export const remove = (req, res) => res.send('Remove User')
 export const login = (req, res) => res.send('Login')
