48c0ae5f173ee40aa2587147a673256b27f7aa60
LeeYoHan <yohan4551@gmail.com>
2021-06-22 01:10:51 +0900

    feat: section10 - 2 수강완료

--- Node/nomad_youtube_clone/wetube/src/controllers/videoController.js
+++ Node/nomad_youtube_clone/wetube/src/controllers/videoController.js
@@ -3,7 +3,9 @@ import Video from '../models/Video'
 
 export const home = async (req, res) => {
   try {
-    const videos = await Video.find({}).sort({ createdAt: 'desc' })
+    const videos = await Video.find({})
+      .sort({ createdAt: 'desc' })
+      .populate('owner')
     return res.render('home', { pageTitle: 'Home', videos })
   } catch (err) {
     return res.render('error')
@@ -108,7 +110,7 @@ export const search = async (req, res) => {
       title: {
         $regex: new RegExp(keyword, 'i')
       },
-    })
+    }).populate('owner')
   }
   return res.render('search', { pageTitle: 'Search', videos })
 }
