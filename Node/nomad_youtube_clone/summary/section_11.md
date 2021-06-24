f03bbb63ce8e4b58763b2cf9c9a1aaba6317c847
LeeYoHan <yohan4551@gmail.com>
2021-06-25 01:22:13 +0900

    feat: section11 - 5 수강완료

--- Node/nomad_youtube_clone/summary/section_11.md
+++ Node/nomad_youtube_clone/summary/section_11.md
@@ -12,3 +12,8 @@
   1. change - 드래그가 끝나야 동작
   2. input - 실시간 동작
 * 볼륨 input과 mute 버튼 연동
+
+## 5. Duration and Current Time
+* loadedmetadata - 비디오가 로드되었을 때 나타나는 메타 데이터
+* timeupdate - 비디오의 현재시간을 받는 이벤트
+* 비디오플레이어의 현재시간과 전체시간 표시 기능 추가
--- Node/nomad_youtube_clone/wetube/src/client/js/videoPlayer.js
+++ Node/nomad_youtube_clone/wetube/src/client/js/videoPlayer.js
@@ -1,8 +1,9 @@
 const video = document.querySelector('video')
 const playBtn = document.getElementById('play')
 const muteBtn = document.getElementById('mute')
-const time = document.getElementById('time')
 const volumeRange = document.getElementById('volume')
+const currentTime = document.getElementById('currentTime')
+const totalTime = document.getElementById('totalTime')
 
 let volumeValue = 0.5
 video.volume = volumeValue
@@ -37,6 +38,16 @@ const handleVolumeChange = (e) => {
   video.volume = value
 }
 
+const handleLoadedMetadata = () => {
+  totalTime.innerText = Math.floor(video.duration)
+}
+
+const handleTimeUpdate = () => {
+  currentTime.innerText = Math.floor(video.currentTime)
+}
+
 playBtn.addEventListener('click', handlePlayClick)
 muteBtn.addEventListener('click', handleMute)
-.addEventListener('input', handleVolumeChange)
+volumeRange.addEventListener('input', handleVolumeChange)
+video.addEventListener('loadedmetadata', handleLoadedMetadata)
+video.addEventListener('timeupdate', handleTimeUpdate)
