bf29fa66bde2e6354778a9a44aa4c63062ce2ffe
LeeYoHan <yohan4551@gmail.com>
2021-06-30 01:24:04 +0900

    feat: section13 - 4 수강완료

--- Node/nomad_youtube_clone/summary/section_13.md
+++ Node/nomad_youtube_clone/summary/section_13.md
@@ -8,3 +8,8 @@
 ## 3. Recording Video
 * stream(영어단어 의미 - 한줄기 강물과도 같은 것)을 녹화해보기 기능 추가
 * 녹화기능을 위해 [MediaRecorder](https://developer.mozilla.org/en-US/docs/Web/API/MediaRecorder)를 사용
+
+## 4. Recording Video part Two
+* URL.createObjectURL - 브라우저 메모리에서만 가능한 URL 생성. 파일을 가리키고있는 URL이라고 생각하면 됨.
+* 녹화한 비디오는 브라우저 메모리에 있다.
+
--- Node/nomad_youtube_clone/wetube/src/client/js/recorder.js
+++ Node/nomad_youtube_clone/wetube/src/client/js/recorder.js
@@ -2,13 +2,14 @@ const startBtn = document.getElementById('startBtn')
 const video = document.getElementById('preview')
 
 let stream = null
+let recorder = null
 
 const init = async () => {
   stream = await navigator.mediaDevices.getUserMedia({
     audio: false,
     video: true,
   })
-  video.srcObject = stream
+  // video.srcObject = stream
   video.play()
 }
 
@@ -19,20 +20,27 @@ const handleStart = () => {
   startBtn.removeEventListener('click', handleStart)
   startBtn.addEventListener('click', handleStop)
 
-  const recorder = new MediaRecorder(stream)
-  recorder.ondataavailable = (e) => console.log('recording done', e)
-  console.log(recorder)
+  recorder = new MediaRecorder(stream)
+  recorder.ondataavailable = (e) => {
+    const videoFile = URL.createObjectURL(e.data)
+    video.srcObject = null
+    video.src = videoFile
+    video.loop = true
+    video.play()
+  }
   recorder.start()
-  console.log(recorder)
-  setTimeout(() => {
-    recorder.stop()
-  }, 10000)
+}
+
+const handleDownload = () => {
+
 }
 
 const handleStop = () => {
-  startBtn.innerText = 'Start Recording'
+  startBtn.innerText = 'download Recording'
   startBtn.removeEventListener('click', handleStop)
-  startBtn.addEventListener('click', handleStart)
+  startBtn.addEventListener('click', handleDownload)
+
+  recorder.stop()
 }
 
 startBtn.addEventListener('click', handleStart)
