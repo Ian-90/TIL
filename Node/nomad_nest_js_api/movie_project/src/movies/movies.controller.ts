cc2ddccb29a2de3b0c3f925a7e33fe91ac021fc8
LeeYoHan <yohan4551@gmail.com>
2021-07-09 01:48:46 +0900

    feat: section_3 - 6 수강완료

--- Node/nomad_nest_js_api/movie_project/src/movies/movies.controller.ts
+++ Node/nomad_nest_js_api/movie_project/src/movies/movies.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get, Post, Delete, Param, Patch, Body, Query } from '@nestjs/common';
 import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
 import { Movie } from './entites/movie.entity';
 import { MoviesService } from './movies.service';
 
@@ -28,7 +29,7 @@ export class MoviesController {
   }
 
   @Patch('/:id')
-  patch(@Param('id') movieId: number, @Body() updateData) {
+  patch(@Param('id') movieId: number, @Body() updateData: UpdateMovieDto) {
     return this.moviesService.update(movieId, updateData)
   }
 }
--- Node/nomad_nest_js_api/summary/section_3.md
+++ Node/nomad_nest_js_api/summary/section_3.md
@@ -71,3 +71,7 @@ app.useGlobalPipes(
   })
 )
 ```
+
+## 6. DTOs and Validation part Two
+* @nestjs/mapped-types - 타입을 변환시키고 사용할 수 있게 하는 패키지
+  * PartialType
