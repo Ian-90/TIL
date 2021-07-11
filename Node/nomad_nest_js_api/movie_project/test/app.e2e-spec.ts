9c003f575d8655326b58eed5884f491eaa8b6977
LeeYoHan <yohan4551@gmail.com>
2021-07-11 18:18:33 +0900

    feat: section_5 - 3 수강완료

--- Node/nomad_nest_js_api/movie_project/test/app.e2e-spec.ts
+++ Node/nomad_nest_js_api/movie_project/test/app.e2e-spec.ts
@@ -68,7 +68,32 @@ describe('AppController (e2e)', () => {
         .get('/movies/999')
         .expect(404)
     })
-    it.todo('DELETE')
-    it.todo('PATCH')
+    
+    it('PATCH 200', () => {
+      return request(app.getHttpServer())
+        .patch('/movies/1')
+        .send({
+          title: 'Updated Test'
+        })
+        .expect(200)
+    })
+
+    it('POST 201', () => {
+      return request(app.getHttpServer())
+        .post('/movies')
+        .send({
+          title: 'Test',
+          year: 2000,
+          genres: ['test'],
+          other: 'thing'
+        })
+        .expect(400)
+    })
+
+    it('DELETE 200', () => {
+      return request(app.getHttpServer())
+        .delete('/movies/1')
+        .expect(200)
+    })
   })
 });
--- Node/nomad_nest_js_api/summary/section_5.md
+++ Node/nomad_nest_js_api/summary/section_5.md
@@ -3,5 +3,8 @@
 * nestjs는 supertest 사용
 
 ## 2. Testing GET movies id
-* `/movies/:id` 관련 테스트 작성
+* `/movies/:id` GET 관련 테스트 작성
 * 테스트도 실제 애플리케이션 환경을 적용해주어야 한다.
+
+## 3. Testing PATCH and DELETE movies id
+* `/movies/:id` PATCH, DELETE 관련 테스트 작성
