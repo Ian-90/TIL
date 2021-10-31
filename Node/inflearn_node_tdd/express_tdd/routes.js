730bad143f0a17f33b3e4f79e2caecdeacc613af
LeeYoHan <yohan4551@gmail.com>
2021-11-01 00:41:07 +0900

    feat: Create 에러 처리 - 2

--- Node/inflearn_node_tdd/express_tdd/routes.js
+++ Node/inflearn_node_tdd/express_tdd/routes.js
@@ -2,6 +2,6 @@ const express = require('express')
 const router = express.Router()
 const productController = require('./controller/products')
 
-router.get('/', productController.hello)
+router.post('/', productController.createProduct)
 
 module.exports = router
