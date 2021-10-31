d83ba0fb260b3f37784149bfc9ad41647884e83e
LeeYoHan <yohan4551@gmail.com>
2021-11-01 00:49:45 +0900

    feat: Create 에러 처리 - 3

--- Node/inflearn_node_tdd/express_tdd/controller/products.js
+++ Node/inflearn_node_tdd/express_tdd/controller/products.js
@@ -1,6 +1,6 @@
 const productModel = require('../models/Product')
 
-exports.createProduct = (req, res, next) => {
-  const createdProduct = productModel.create(req.body)
+exports.createProduct = async (req, res, next) => {
+  const createdProduct = await productModel.create(req.body)
   res.status(201).json(createdProduct)
 }
--- Node/inflearn_node_tdd/express_tdd/test/unit/products.test.js
+++ Node/inflearn_node_tdd/express_tdd/test/unit/products.test.js
@@ -23,20 +23,20 @@ describe('Product Controller Create', () => {
     expect(typeof productController.createProduct).toBe('function')
   })
 
-  it('should call ProductModel.create', () => {
-    productController.createProduct(req, res, next)
+  it('should call ProductModel.create', async () => {
+    await productController.createProduct(req, res, next)
     expect(productModel.create).toBeCalledWith(newProduct)
   })
 
-  it('should return 201 response code', () => {
-    productController.createProduct(req, res, next)
+  it('should return 201 response code', async () => {
+    await productController.createProduct(req, res, next)
     expect(res.statusCode).toBe(201)
     expect(res._isEndCalled()).toBeTruthy()
   })
 
-  it('should return json body in response', () => {
+  it('should return json body in response', async () => {
     productModel.create.mockReturnValue(newProduct)
-    productController.createProduct(req, res, next)
+    await productController.createProduct(req, res, next)
     expect(res._getJSONData()).toStrictEqual(newProduct)
   })
 })
