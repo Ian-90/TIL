480f86597d4c745be035e2cce5ee22240680a1a1
LeeYoHan <yohan4551@gmail.com>
2021-10-31 23:46:08 +0900

    feat: Create 단위 테스트 작성 - 7

--- Node/inflearn_node_tdd/express_tdd/controller/products.js
+++ Node/inflearn_node_tdd/express_tdd/controller/products.js
@@ -1,6 +1,6 @@
 const productModel = require('../models/Product')
 
 exports.createProduct = (req, res, next) => {
-  productModel.create(req.body)
-  res.status(201).send()
+  const createdProduct = productModel.create(req.body)
+  res.status(201).json(createdProduct)
 }
--- Node/inflearn_node_tdd/express_tdd/test/unit/products.test.js
+++ Node/inflearn_node_tdd/express_tdd/test/unit/products.test.js
@@ -33,4 +33,10 @@ describe('Product Controller Create', () => {
     expect(res.statusCode).toBe(201)
     expect(res._isEndCalled()).toBeTruthy()
   })
+
+  it('should return json body in response', () => {
+    productModel.create.mockReturnValue(newProduct)
+    productController.createProduct(req, res, next)
+    expect(res._getJSONData()).toStrictEqual(newProduct)
+  })
 })
