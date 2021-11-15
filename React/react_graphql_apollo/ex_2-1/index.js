aa27bca53b807f793104bb57d30f43897fd86864
LeeYoHan <yohan4551@gmail.com>
2021-11-15 22:21:09 +0900

    feat: 2. GraphQL 서버 만들어보기 - 1

---
+++ React/react_graphql_apollo/ex_2-1/index.js
@@ -0,0 +1,25 @@
+const database = require('./database')
+const { ApolloServer, gql } = require('apollo-server')
+const typeDefs = gql`
+  type Query {
+    teams: [Team]
+  }
+  type Team {
+    id: Int
+    manager: String
+    office: String
+    extension_number: String
+    mascot: String
+    cleaning_duty: String
+    project: String
+  }
+`
+const resolvers = {
+  Query: {
+    teams: () => database.teams
+  }
+}
+const server = new ApolloServer({ typeDefs, resolvers })
+server.listen().then(({ url }) => {
+console.log(`🚀  Server ready at ${url}`)
+})
---
+++ React/react_graphql_apollo/section_2.md
@@ -0,0 +1,11 @@
+## 1. Apollo 서버 구축하기
+* `ex_2-1` 폴더에 구현
+* typeDef
+  * GraphQL 명세에서 사용될 데이터, 요청의 타입 지정
+  * `gql(template literal tag)로 생성됨
+* resolvers
+  * 서비스의 액션들을 함수로 지정
+  * 요청에 따라 데이터를 반환, 입력, 수정, 삭제
+* GraphQL Playground
+  * 작성한 GraphQL type, resolver 명세 확인
+  * 데이터 요청 및 전송 테스트
