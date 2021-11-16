006c9fc970396e4a5acaa02f4ba392e5b9cf6cb3
LeeYoHan <yohan4551@gmail.com>
2021-11-17 00:12:44 +0900

    feat: type annotations - variable

--- JavaScript/udemy_typescript_appendix/README.md
+++ JavaScript/udemy_typescript_appendix/README.md
@@ -1,5 +1,16 @@
 # 유데미 타입스크립트 부록강의
 
-* `Typescript` = `Javascript` + `A type system`
-* Type annotations - 우리가 타입스크립트에게 알려주는 타입
-* Type inference - 타입스크립트가 추론하는 타입
+### `Typescript` = `Javascript` + `A type system`
+### 1. Type annotations - 우리가 타입스크립트에게 알려주는 타입
+* 타입 선언을 사용하는 경우
+  * 변수
+    1. 함수가 any 타입을 return 하거나 value를 명확하게 해야하는 경우
+    2. 한줄로 변수 선언 후, 나중에 초기화 하는 경우
+    3. 추론할 수 없는 타입을 가진 변수에 타입을 주기를 원할 때
+
+### 2. Type inference - 타입스크립트가 추론하는 타입
+```ts
+// variable declaration = variable Initialization
+const color = 'red'
+```
+* 타입 추론을 사용하는 경우 - 대부분
--- JavaScript/udemy_typescript_appendix/ex_2_features/annotations/variable.ts
+++ JavaScript/udemy_typescript_appendix/ex_2_features/annotations/variable.ts
@@ -29,3 +29,37 @@ let point: { x: number; y: number } = {
 const logNumber: (i: number) => void = (i: number) => {
   console.log(i)
 }
+
+// 타입추론
+let num = 5;
+
+// 추론이 안되는 경우
+let getItem;
+getItem = false
+
+//1. Function that returns the 'any' type
+const json = '{"x": 10, "y": 20}'
+const coordinates: { x: number; y: number } = JSON.parse(json) // any type
+console.log(coordinates) // { x: 10, y: 10 }
+
+coordinates.asdffd // any 타입이면 타입스크립트가 타입에러를 잡지 못함
+
+// 2. When we declare a variable on one line and initializate it later
+let words = ['red', 'green', 'blue']
+let foundWord: boolean;
+
+for (let i = 0; i < words.length; i++) {
+  if (words[i] === 'green') {
+    foundWord = true
+  }
+}
+
+// 3. Variabel hose type cannot be infferred correctly
+let numbers = [-10, -1, 12]
+let numberAboveZero: boolean | number = false
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] > 0) {
+    numberAboveZero = numbers[i]
+  }
+}
