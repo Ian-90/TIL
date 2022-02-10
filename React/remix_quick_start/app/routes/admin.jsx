1ad53aab77ec3601f9f999f951e0d110b973bbac
LeeYoHan <yohan4551@gmail.com>
2022-02-11 00:18:33 +0900

    feat: creating blog posts

---
+++ React/remix_quick_start/app/routes/admin.jsx
@@ -0,0 +1,32 @@
+import { Link, useLoaderData } from "remix";
+import adminStyles from "~/styles/admin.css";
+import { getPosts } from "~/post";
+
+export const loader = async () => {
+  return getPosts();
+};
+
+export const links = () => {
+  return [{ rel: "stylesheet", href: adminStyles }];
+};
+
+export default function Admin() {
+  const posts = useLoaderData();
+  return (
+    <div className="admin">
+      <nav>
+        <h1>Admin</h1>
+        <ul>
+          {posts.map(post => (
+            <li key={post.slug}>
+              <Link to={`/posts/${post.slug}`}>
+                {post.title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+      <main>...</main>
+    </div>
+  );
+}
