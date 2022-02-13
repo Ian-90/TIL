38dbf4e498cae256fa9081634fafb326600abbbe
LeeYoHan <yohan4551@gmail.com>
2022-02-13 22:54:38 +0900

    feat: add Styling tutorial

--- React/remix-jokes/app/root.tsx
+++ React/remix-jokes/app/root.tsx
@@ -6,10 +6,33 @@ import {
   Scripts,
   ScrollRestoration
 } from "remix";
-import type { MetaFunction } from "remix";
+import type { MetaFunction, LinksFunction } from "remix";
 
 export const meta: MetaFunction = () => {
   return { title: "New Remix App" };
+};
+
+import globalStylesUrl from "./styles/global.css";
+import globalMediumStylesUrl from "./styles/global-medium.css";
+import globalLargeStylesUrl from "./styles/global-large.css";
+
+export const links: LinksFunction = () => {
+  return [
+    {
+      rel: "stylesheet",
+      href: globalStylesUrl
+    },
+    {
+      rel: "stylesheet",
+      href: globalMediumStylesUrl,
+      media: "print, (min-width: 640px)"
+    },
+    {
+      rel: "stylesheet",
+      href: globalLargeStylesUrl,
+      media: "screen and (min-width: 1024px)"
+    }
+  ];
 };
 
 export default function App() {
