dbf92e377c3dc80c4cf7fd868d67f5d082a42fa5
LeeYoHan <yohan4551@gmail.com>
2022-02-19 00:31:20 +0900

    feat: add Authentication tutorial

--- React/remix-jokes/app/routes/jokes/new.tsx
+++ React/remix-jokes/app/routes/jokes/new.tsx
@@ -2,6 +2,7 @@ import type { ActionFunction } from "remix";
 import { redirect, json, useActionData } from "remix";
 
 import { db } from "~/utils/db.server";
+import { requireUserId } from "~/utils/session.server";
 
 function validateJokeContent(content: string) {
   if (content.length < 10) {
@@ -33,6 +34,7 @@ const badRequest = (data: ActionData) =>
 export const action: ActionFunction = async ({
   request
 }) => {
+  const userId = await requireUserId(request);
   const form = await request.formData();
   const name = form.get("name");
   const content = form.get("content");
@@ -58,7 +60,9 @@ export const action: ActionFunction = async ({
     return badRequest({ fieldErrors, fields });
   }
 
-  const joke = await db.joke.create({ data: fields });
+  const joke = await db.joke.create({
+    data: { ...fields, jokesterId: userId }
+  });
   return redirect(`/jokes/${joke.id}`);
 };
 
