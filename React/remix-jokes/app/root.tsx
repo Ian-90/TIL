d08a65cd4c53ca3aaf03accec50ffea2ae7ce4d1
LeeYoHan <yohan4551@gmail.com>
2022-02-20 00:15:45 +0900

    feat: add Javascript... tutorial

--- React/remix-jokes/app/root.tsx
+++ React/remix-jokes/app/root.tsx
@@ -4,7 +4,8 @@ import {
   LiveReload,
   Outlet,
   useCatch,
-  Meta
+  Meta,
+  Scripts
 } from "remix";
 
 import globalStylesUrl from "./styles/global.css";
@@ -61,6 +62,7 @@ function Document({
       </head>
       <body>
         {children}
+        <Scripts />
         <LiveReload />
       </body>
     </html>
@@ -92,6 +94,8 @@ export function CatchBoundary() {
 }
 
 export function ErrorBoundary({ error }: { error: Error }) {
+  console.error(error);
+
   return (
     <Document title="Uh-oh!">
       <div className="error-container">
