c3d99b22d4656a1cf2fcf67983228f6521df7c4a
LeeYoHan <yohan4551@gmail.com>
2022-04-03 22:29:40 +0900

    feat: 1-6 ~ 1-9

--- Node/nomad_zoom_clone/zoom/src/public/js/app.js
+++ Node/nomad_zoom_clone/zoom/src/public/js/app.js
@@ -1,3 +1,12 @@
+const messageList = document.querySelector('ul')
+const nikcForm = document.querySelector('#nick')
+const messageForm = document.querySelector('#message')
+
+function makeMessage(type, payload) {
+  const msg = { type, payload }
+  return JSON.stringify(msg)
+}
+
 const socket = new WebSocket(`ws://${window.location.host}`)
 
 socket.addEventListener('open', () => {
@@ -6,6 +15,9 @@ socket.addEventListener('open', () => {
 
 socket.addEventListener('message', (message) => {
   console.log('New message: ', message.data)
+  const li = document.createElement('li')
+  li.innerHTML = message.data
+  messageList.append(li)
 })
 
 socket.addEventListener('close', () => {
@@ -15,3 +27,23 @@ socket.addEventListener('close', () => {
 setTimeout(() => {
   socket.send('hello from the browser')
 }, 10000)
+
+function handleSubmit(event) {
+  event.preventDefault()
+  const input = messageForm.querySelector('input')
+  socket.send(makeMessage('new_message', input.value))
+  const li = document.createElement('li')
+  li.innerText = `You: ${input.value}`
+  message.append(li)
+  input.value = ''
+}
+
+function handleNickSubmit(event) {
+  event.preventDefault()
+  const input = nikcForm.querySelector('input')
+  socket.send(makeMessage('nickname', input.value))
+  input.value = ''
+}
+
+messageForm.addEventListener('submit', handleSubmit)
+nikcForm.addEventListener('submit', nickSubmit)
--- Node/nomad_zoom_clone/zoom/src/server.js
+++ Node/nomad_zoom_clone/zoom/src/server.js
@@ -14,12 +14,23 @@ const handleListen = () => console.log('Listening on http://localhost:3000')
 
 const server = http.createServer(app)
 const wss = new WebSocket.WebSocketServer({ server })
+// 같은 메세지를 여러명에게 보내기 위해 사용(실제로는 DB연동이 필요)
+const sockets = []
 
 wss.on('connection', (socket) => {
+  sockets.push(socket)
+  socket['nickname'] = 'Anon'
   console.log('Connected to Browser')
 
-  socket.on('message', (message) => {
-    console.log(message.toString())
+  socket.on('message', (msg) => {
+    console.log(msg.toString())
+    const message = JSON.parse(msg)
+    switch(parsed.type) {
+      case "new_message":
+        sockets.forEach(aSocket => aSocket.send(`${socket.nickname}: ${message.payload.toString()}`))
+      case "nickname":
+        socket['nickname'] = message.payload
+    }
   })
 
   socket.on('close', () => {
